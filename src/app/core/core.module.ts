dcd68f529e0a135d0f6f446c71a1ca8c56b3371a
cTahirih <tahysluana@gmail.com>
2019-06-09 18:04:57 -0500

    Create a header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule,
+         MatButtonModule,
+         MatSidenavModule,
+         MatIconModule,
+         MatListModule 
+       } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+
+/**
+ * Components
+ */
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class CoreModule { }
