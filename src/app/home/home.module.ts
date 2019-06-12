43c147dea818ed8217f97767adc4e2b054dfcd11
cTahirih <tahysluana@gmail.com>
2019-06-11 21:34:16 -0500

    Create component cards in home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+/*
+Components
+*/
+import { CardsComponent } from './cards/cards.component';
+import { HomeComponent } from './home.component';
 @NgModule({
-  declarations: [],
+  declarations: [
+    CardsComponent,
+    HomeComponent
+  ],
   imports: [
     CommonModule
   ]
