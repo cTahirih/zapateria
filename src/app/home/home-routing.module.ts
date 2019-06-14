beb50b3225cab130c4b3b89d8505b7c0ecb088ec
Cintia Tahirih Jaliri <32286691+cTahirih@users.noreply.github.com>
2019-06-14 12:58:48 -0500

    Remove routing in this module

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,23 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 /*
   Components
 * */
-import { HomeComponent } from './home.component';
-import { ListClientComponent } from '../list-products/list-client/list-client.component';
-import { SalesComponent } from '../sales/sales.component';
-import { NotFoundComponent } from '../core/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'clients', component: ListClientComponent },
-  { path: 'sales', component: SalesComponent },
-  {
-    path: 'not-found',
-    component: NotFoundComponent
-  },
-  {
-    path: '**',
-    redirectTo: 'not-found'
-  }
+
 ];
 
 @NgModule({
