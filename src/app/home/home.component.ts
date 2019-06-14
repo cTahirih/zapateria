e98e1231a6226794ac944e7fb6321a7f08f6aa69
Cintia Tahirih Jaliri <32286691+cTahirih@users.noreply.github.com>
2019-06-14 14:33:44 -0500

    refactor component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  BreakpointObserver,
-  Breakpoints
-} from '@angular/cdk/layout';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
@@ -13,12 +8,6 @@ import {map} from 'rxjs/operators';
 })
 
 export class HomeComponent {
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
   public data = [
     {
       iconClass: 'far fa-money-bill-alt',
@@ -41,4 +30,6 @@ export class HomeComponent {
       mount: '$0,00'
     }
   ];
+  constructor() {}
+
 }
