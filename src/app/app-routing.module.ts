9ba1a84b39a9d101062e702292fc9091aa8d34bd
Cintia Tahirih Jaliri <32286691+cTahirih@users.noreply.github.com>
2019-06-14 13:04:48 -0500

    Update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+/*
+  Components
+* */
+import { HomeComponent } from './home/home.component';
+import { ListClientComponent } from './list-products/list-client/list-client.component';
+import { SalesComponent } from './sales/sales.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'clients',
+    component: ListClientComponent,
+    children: []
+  },
+  { path: 'sales', component: SalesComponent },
+  {
+    path: 'not-found',
+    component: NotFoundComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'not-found'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
