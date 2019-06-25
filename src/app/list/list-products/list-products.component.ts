dad1b996e7b5c865ada44c93aa02d9d88a1ce743
Cintia Tahirih Jaliri <32286691+cTahirih@users.noreply.github.com>
2019-06-25 17:58:09 -0500

    change subscription to get list products

--- src/app/list/list-products/list-products.component.ts
+++ src/app/list/list-products/list-products.component.ts
@@ -1,5 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ListProductsService } from '../services/list-products.service';
+import {tap} from 'rxjs/operators';
+
+export interface IProduct {
+  id: number;
+  nombre: string;
+  precio: number;
+  sku: string;
+  stock: number;
+}
 
 @Component({
   selector: 'app-list-products',
@@ -7,22 +17,36 @@ import { ListProductsService } from '../services/list-products.service';
   styleUrls: ['./list-products.component.scss']
 })
 export class ListProductsComponent implements OnInit {
-  listProducts: any;
+
+  listProducts: IProduct[];
+  displayedColumns: string[] = ['position', 'name', 'price', 'stock'];
+  paginador: any;
+  pages: number[];
   constructor(
-    private _listProductService: ListProductsService
+    private listProductService: ListProductsService,
+    private activatedRoute: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this._listProductService.getListProducts()
-    .then((products: any) => {
-      console.log(products.content);
-      this.listProducts = products.content;
-    }).catch((error) => {
-      console.log('Error:' + error);
+    this.activatedRoute.paramMap.subscribe(params => {
+      let page: number = +params.get('page');
+      if (!page) {
+        page = 1;
+      }
+      this.listProductService.getListProducts(page)
+        .pipe(
+          tap((response: any) => {
+            (response.content as IProduct[]).forEach(producto => {
+              console.log(producto);
+            });
+          })
+        ).subscribe(response => {
+          this.listProducts = response.content as IProduct[];
+          this.paginador = response;
+          this.pages = new Array(response.totalPages).fill(1).map((value, index) => index + 1);
+      });
     });
   }
 
-  showConfig() {
-
-  }
 }
+
