88f8b52c3d8497aa081878004405579d6a78b8b0
Cintia Tahirih Jaliri <32286691+cTahirih@users.noreply.github.com>
2019-06-25 18:01:01 -0500

    change form to get list product using observables

--- src/app/list/services/list-products.service.ts
+++ src/app/list/services/list-products.service.ts
@@ -1,17 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map, tap} from 'rxjs/operators';
+export interface IProduct {
+  id: number;
+  nombre: string;
+  precio: number;
+  sku: string;
+  stock: number;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class ListProductsService {
-
+  private URL_PRODUCTS =  'https://pos-zapateria.herokuapp.com/api/productos';
   constructor(
-    private _http: HttpClient
+    private HTTP: HttpClient
   ) {
     console.log('setvicio de productos');
   }
 
-  getListProducts() {
-    return this._http.get('https://pos-zapateria.herokuapp.com/api/productos/page/1').toPromise();
+  getListProducts(page: number): Observable<any> {
+    return this.HTTP.get(this.URL_PRODUCTS + '/page/' + page).pipe(
+      tap((response: any) => {
+        (response.content as IProduct[]).forEach(product => {
+          console.log(product);
+        });
+      }),
+      map((response: any) => {
+        (response.content as IProduct[]).map(product => {
+          return product;
+        });
+        return response;
+      }),
+      tap((response: any) => {
+        (response.content as IProduct[]).forEach(producto => {
+          console.log(producto);
+        });
+      })
+    );
   }
 }
