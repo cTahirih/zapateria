55da9cfe11f1c5c9a47fa430013ee5475f0206d9
cTahirih <tahysluana@gmail.com>
2019-06-30 19:51:18 -0500

    create new route for clients

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     component: ListClientComponent
   },
   {
+    path: 'clients/page/:page',
+    component: ListClientComponent
+  },
+  {
     path: 'products',
     component: ListProductsComponent
   },
