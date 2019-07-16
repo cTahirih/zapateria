8974e6767b6e63363e83bd3f74c7ac40894d5ace
cTahirih <tahysluana@gmail.com>
2019-07-16 12:06:26 -0500

    styles page 404

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { ErrorMessageComponent } from './error-message/error-message.component';
+import { HomeRoutingModule } from '../home/home-routing.module';
 /**
  * Components
  */
 
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, ErrorMessageComponent],
   imports: [
     CommonModule,
-    LayoutModule
-
+    LayoutModule,
+    HomeRoutingModule
   ],
   exports: [
   ]
